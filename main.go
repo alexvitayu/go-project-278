b5159292155faeec32320a96d4a7a707aac271d8
Aliaksei Yuryeu <alexvitayu1212@gmail.com>
2025-12-20 17:39:19 +0300

    Launch project

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.Use(gin.Recovery())
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	if err := r.Run("0.0.0.0:8080"); err != nil {
+		log.Fatalf("не удалось запустить сервер: %v", err)
+	}
+}
